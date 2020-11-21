c69e4008c4a795671f5d50e740240cdf2fdb9138
sberkaye <berkayergin99@gmail.com>
2020-11-21 16:40:26 +0300

    Made scroll animations with animate.css and jQuery

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,12 +13,57 @@ $(document).ready(function () {
       offset: "56px", // 20px(font) + 18px(margin) + 16px(padding) + 2px(box-shadow)
     }
   );
-  // var waypoints = $("#handler-first").waypoint(
-  //   function (direction) {
-  //     notify(this.element.id + " hit 25% from top of window");
-  //   },
-  //   {
-  //     offset: "25%",
-  //   }
-  // );
+  // scroll on buttons
+  $(".js--scroll-to-plans").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-plans").offset().top },
+      1000
+    );
+  });
+  $(".js--scroll-to-features").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-features").offset().top },
+      500
+    );
+  });
+  // scroll animations
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass(
+        "animate__animated animate__fadeIn animate__faster"
+      );
+    },
+    {
+      offset: "70%",
+    }
+  );
+
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass(
+        "animate__animated animate__fadeInUp animate__fast"
+      );
+    },
+    {
+      offset: "70%",
+    }
+  );
+
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animate__animated animate__fadeIn");
+    },
+    {
+      offset: "70%",
+    }
+  );
+
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--plan-box-premium").addClass("animate__animated animate__pulse");
+    },
+    {
+      offset: "40%",
+    }
+  );
 });
