51405176363f38cf1aa28b99cc1d3f9c761366d0
sberkaye <berkayergin99@gmail.com>
2020-11-21 15:53:31 +0300

    Used jQuery and waypoints to make the sticky nav appear and disappear

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
-  $("h1").click(function () {
-    $(this).css("background-color", "#ff0000");
-  });
+  // if user is scrolling down, start showing the sticky nav
+  // else, (if the user is scrolling up) remove the sticky nav
+  $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction === "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "56px", // 20px(font) + 18px(margin) + 16px(padding) + 2px(box-shadow)
+    }
+  );
+  // var waypoints = $("#handler-first").waypoint(
+  //   function (direction) {
+  //     notify(this.element.id + " hit 25% from top of window");
+  //   },
+  //   {
+  //     offset: "25%",
+  //   }
+  // );
 });
